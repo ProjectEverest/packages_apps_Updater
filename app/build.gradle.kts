5370ae5aec117e8ee831ce80b2b20d43c3f49b0b
janak <niraulajanak2019@gmail.com>
2024-08-25 22:45:05 +0545

    Updater: Adapt for EverestOS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 import java.util.Properties
-import org.lineageos.generatebp.GenerateBpPlugin
-import org.lineageos.generatebp.GenerateBpPluginExtension
-import org.lineageos.generatebp.models.Module
+import com.everest.generatebp.GenerateBpPlugin
+import com.everest.generatebp.GenerateBpPluginExtension
+import com.everest.generatebp.models.Module
 
 plugins {
     id("com.android.application")
@@ -18,7 +18,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.lineageos:gradle-generatebp:+")
+        classpath("com.everest:gradle-generatebp:+")
     }
 }
 
@@ -33,7 +33,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "org.lineageos.updater"
+        applicationId = "com.everest.updater"
         minSdk = 30
         targetSdk = 33
         versionCode = 1
